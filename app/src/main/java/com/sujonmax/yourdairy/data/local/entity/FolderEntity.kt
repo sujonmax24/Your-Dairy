96891d9d65d82e031040a39623a54bd8d2867eb3
Sujon Mia <123140537+sujonmax24@users.noreply.github.com>
2026-08-23 22:04:21 +0600

    Add Room folder entity

---
+++ app/src/main/java/com/sujonmax/yourdairy/data/local/entity/FolderEntity.kt
@@ -0,0 +1,12 @@
+package com.sujonmax.yourdairy.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "folders")
+data class FolderEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val createdAt: Long = System.currentTimeMillis()
+)
